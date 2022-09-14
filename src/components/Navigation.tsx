f47b9c7cf5481109e5649a9d16a764ad62e6323e
Tariel <ermekovtariel@gmail.com>
2022-09-15 02:10:10 +0600

    fix Navigation title GITHUB/REPOS

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,7 +5,7 @@ export function Navigation() {
   return (
     <>
       <nav className='flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white'>
-        <h3 className='font-bold'>ELMA</h3>
+        <h3 className='font-bold'>GITHUB/REPOS</h3>
         <span>
           <Link to={'/'} className='mr-2'>
             Home
